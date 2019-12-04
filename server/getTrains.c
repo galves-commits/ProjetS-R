1c2c6daee840b3daff7441259b48296c61c4f0df
Axel NOWAK <hytock@DESKTOP-PQHQ8A6.localdomain>
2019-12-04 10:16:52 +0100

    getreduc

--- server/getTrains.c
+++ server/getTrains.c
@@ -71,7 +71,8 @@ Cellule * getTHTrains(char *depart, char *arrivee, char* heure ,Cellule *trains)
 Cellule * getTBTrains(char *depart, char *arrivee, char* heure ,Cellule *trains){
     Cellule *t = trains;
     Cellule *bonTrains = malloc(sizeof(Cellule));
-    char * h1,h2;
+    char * h1;
+    char * h2;
     sscanf(heure,"%s-%s",h1,h2);
     Temps hDebReq = stringToTemps(h1);
     Temps hFinReq = stringToTemps(h2);
