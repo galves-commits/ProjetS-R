22ad9db58f76c8461157c7aad65c840fbcd85ca4
Axel NOWAK <hytock@DESKTOP-PQHQ8A6.localdomain>
2019-12-04 09:33:38 +0100

    trains.h

--- headers/server.h
+++ headers/server.h
@@ -1,33 +1,3 @@
-#include <stdlib.h>
-#include <stdbool.h>
-
-typedef struct temps
-{
-    int heure;
-    int minute;
-}Temps;
-
-typedef struct train
-{
-    int numero;
-    char *villeDepart;
-    char *villeArrivee;
-    Temps heureDep;
-    Temps heureArr;
-    double prix;
-    char *reduc;
-}Train;
-
-typedef struct cellule{
-    Train leTrain;
-    struct cellule* suivant;
-}Cellule;
-typedef Cellule * PCellule;
-//inserTete(&(dernier->suivant), train)
-
-int inferieur(Temps t1, Temps t2);
-int superieur(Temps t1, Temps t2);
-
 void inserTete(Cellule** , Train);
 
 Cellule * traiterFic();
@@ -37,5 +7,4 @@ void requeteTrajet(int, Cellule*);
 void echangeClient(int, Cellule*);
 void reponseRequete(Cellule * trains, int echange);
 Cellule * getTrains(char* , char* , char* , Cellule* );
-Temps stringToTemps(char*);
-Temps duree(Train );
+
