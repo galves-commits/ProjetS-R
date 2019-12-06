66cf62acd34fe0d06fa06b8844fd0222ad939e01
Axel NOWAK <garmemail@yahoo.fr>
2019-12-06 20:28:58 +0100

    commentaire suite

--- server/cellule.c
+++ server/cellule.c
@@ -3,8 +3,12 @@
 
 
 void inserTete(Cellule ** tete, Train t){
+    //creation de la nouvelle cellule
     Cellule *nouv = malloc(sizeof(Cellule));
+    //on lui attribut le train
     nouv->leTrain=t;
+    //l'ancienne tete de train est attribuée en tant que cellule suivante de la nouvelle cellule
     nouv->suivant = *tete;
+    //la nouvelle cellule devient la tete de la chaine
     *tete = nouv;
 }
